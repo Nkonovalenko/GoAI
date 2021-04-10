d5d7dfbb3628387c0e59b810ff6e2c1c76b53e08
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-09 22:53:59 -0400

    Create Point class

--- dlgo/gotypes.py
+++ dlgo/gotypes.py
@@ -9,3 +9,15 @@ class Player(enum.Enum):
 	@property
 	def other(self):
 		return Player.black if self == Player.white else Player.white
+
+class Point(namedtuple('Point', 'row col')):
+	"""This class represents a point on the board."""
+	# namedtuple allows us to access point.row and point.col
+	# 	instead of point[0] and point[1]
+	def neighbors(self):
+		return [
+			Point(self.row - 1, self.col),
+			Point(self.row + 1, self.col),
+			Point(self.row, self.col - 1),
+			Point(self.row, self.col + 1),
+		]
