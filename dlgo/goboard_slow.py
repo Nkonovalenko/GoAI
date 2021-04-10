832d2a313a3ffd60af7bd4bd6048c8ab0c817032
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-09 23:15:35 -0400

    Create GoString class
    Create a class that tracks strings, groups of connected stones of the same color. This allows for efficient tracking of liberties using a set, as opposed to required a Depth First Search of the grid each time.

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -2,26 +2,62 @@ import copy
 from dlgo.gotypes import Player
 
 class Move():
-	"""Class handling any action a player can play."""
-	def __init__(self, point=None, is_pass=False, is_resign=False):
-		"""Initilize object."""
-		assert (point is nont None) ^ is_pass ^ is_resign
-		self.point = point
-		self.is_play = (self.point is not None)
-		self.is_pass = is_pass
-		self.is_resign = is_resign
+    """Class handling any action a player can play."""
+    def __init__(self, point=None, is_pass=False, is_resign=False):
+        """Initilize a move."""
+        assert (point is nont None) ^ is_pass ^ is_resign
+        self.point = point
+        self.is_play = (self.point is not None)
+        self.is_pass = is_pass
+        self.is_resign = is_resign
 
-	@classmethod
-	def play(cls, point):
-		"""This move places a stone on the board."""
-		return Move(point=point)
+    @classmethod
+    def play(cls, point):
+        """This move places a stone on the board."""
+        return Move(point=point)
 
-	@classmethod
-	def pass_turn(cls):
-		"""This move passes."""
-		return Move(is_pass=True)
+    @classmethod
+    def pass_turn(cls):
+        """This move passes."""
+        return Move(is_pass=True)
 
-	@classmethod
-	def resign(cls):
-		"""This move resigns the current game."""
-		return Move(is_resign=True)
+    @classmethod
+    def resign(cls):
+        """This move resigns the current game."""
+        return Move(is_resign=True)
+
+class GoString():
+    """A chain of connected stones of the same color."""
+    def __init__(self, color, stones, liberties):
+        """Initilize a GoString."""
+        self.color = color
+        self.stones = set(stones)
+        self.liberties = set(liberties)
+
+    def remove_liberty(self, point):
+        """Remove a liberty."""
+        self.liberties.remove(point)
+
+    def add_liberty(self, point):
+        """Add a liberty."""
+        self.liberties.add(point)
+
+    def merged_with(self, go_string):
+        """Retrun a new GoString containing all stones in both strings."""
+        assert go_string.color == self.color
+        combined_stones = self.stones | go_string.stones
+        return GoString(
+            self.color, combined_stones,
+            (self.liberties | go_string.liberties) - combined_stones)
+
+    @property
+    def num_liberties(self):
+        """Return the number of liberties."""
+        return len(self.liberties)
+
+    def __eq__(self, other):
+        """Check if both GoStrings are the same."""
+        return isinstance(other, GoString) and \
+            self.color == other.color and \
+            self.stones == other.stones and \
+            self.liberties == other.liberties
--- dlgo/gotypes.py
+++ dlgo/gotypes.py
@@ -2,22 +2,22 @@ import enum
 from collections import namedtuple
 
 class Player(enum.Enum):
-	"""This class represents the player."""
-	black = 1
-	white = 2
+    """This class represents the player."""
+    black = 1
+    white = 2
 
-	@property
-	def other(self):
-		return Player.black if self == Player.white else Player.white
+    @property
+    def other(self):
+        return Player.black if self == Player.white else Player.white
 
 class Point(namedtuple('Point', 'row col')):
-	"""This class represents a point on the board."""
-	# namedtuple allows us to access point.row and point.col
-	# 	instead of point[0] and point[1]
-	def neighbors(self):
-		return [
-			Point(self.row - 1, self.col),
-			Point(self.row + 1, self.col),
-			Point(self.row, self.col - 1),
-			Point(self.row, self.col + 1),
-		]
+    """This class represents a point on the board."""
+    # namedtuple allows us to access point.row and point.col
+    #   instead of point[0] and point[1]
+    def neighbors(self):
+        return [
+            Point(self.row - 1, self.col),
+            Point(self.row + 1, self.col),
+            Point(self.row, self.col - 1),
+            Point(self.row, self.col + 1),
+        ]
