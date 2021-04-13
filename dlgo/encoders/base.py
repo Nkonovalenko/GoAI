bdb51b1b6add2b09e790aa4bf54d2f26ef26d08a
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-12 23:09:38 -0400

    Update base.py
    Added function that allows for creation of various encoders by passing in the name

--- dlgo/encoders/base.py
+++ dlgo/encoders/base.py
@@ -30,3 +30,11 @@ class Encoder:
     def shape(self):
         """Return shape of encoded board structure."""
         raise NotImplementedError()
+
+def get_encoder_by_name(name, board_size):
+    """Create encoder instances by referencing name."""
+    if isinstance(board_size, int):
+        board_size = (board_size, board_size)
+    module = importlib.import_module('dlgo.encoders.' + name)
+    constructor = getattr(module, 'create')
+    return constructor(board_size)
