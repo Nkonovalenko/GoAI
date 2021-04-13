d063ee210dba54dfb507264d99fa8970c6cae6db
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-12 23:26:32 -0400

    Finish oneplane encoder

--- dlgo/encoders/oneplane.py
+++ dlgo/encoders/oneplane.py
@@ -34,3 +34,17 @@ class OnePlaneEncoder(Encoder):
     def encode_point(self, point):
         """Encode a single point."""
         return self.board_width * (point.row - 1) + (point.col - 1)
+
+    def decode_point_index(self, index):
+        """Decode index into point on board."""
+        row = index // self.board_width
+        col = index % self.board_width
+        return Point(row=r + 1, col=c + 1)
+
+    def num_points(self):
+        """Return number of points on board."""
+        return self.board_height * self.board_width
+
+    def shape(self):
+        """Return shape of encoder."""
+        return self.num_planes, self.board_height, self.board_width
