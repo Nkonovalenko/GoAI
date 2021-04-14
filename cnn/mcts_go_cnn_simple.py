4325b3a0573381f63fa02cdc20504f5428f1c656
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-13 23:52:08 -0400

    Load data

--- cnn/mcts_go_cnn_simple.py
+++ cnn/mcts_go_cnn_simple.py
@@ -5,3 +5,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Conv2D, Flatten
 
+np.random.seed(123)
+X = np.load('../generated_games/features-200.npy')
+Y = np.load('../generated_games/labels-200.npy')
+
