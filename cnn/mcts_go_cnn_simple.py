59b4c44e74bc7beb6ad681f7b23c12c36c4da5bc
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-13 23:52:44 -0400

    Split data

--- cnn/mcts_go_cnn_simple.py
+++ cnn/mcts_go_cnn_simple.py
@@ -9,3 +9,13 @@ np.random.seed(123)
 X = np.load('../generated_games/features-200.npy')
 Y = np.load('../generated_games/labels-200.npy')
 
+samples = X.shape[0]
+size = 9
+input_shape = (size, size, 1)
+
+X = X.reshape(samples, size, size, 1)
+
+train_samples = 10000
+X_train, X_test = X[:train_samples], X[train_samples:]
+Y_train, Y_test = Y[:train_samples], Y[train_samples:]
+
