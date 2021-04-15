f771b465e7a5004d5f7e6cbc3170d2251abda919
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-15 00:24:39 -0400

    Load in data

--- cnn/mcts_go_cnn.py
+++ cnn/mcts_go_cnn.py
@@ -1,1 +1,8 @@
 import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+
+np.random.seed(123)
+X = np.load('../generated_games/features-200.npy')
+Y = np.load('../generated_games/labels-200.npy')
