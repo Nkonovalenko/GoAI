08ee6a6071431912abc29ff10d2176abfa4dd8ce
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-15 00:26:52 -0400

    Add first few layers

--- cnn/mcts_go_cnn.py
+++ cnn/mcts_go_cnn.py
@@ -16,3 +16,8 @@ X = X.reshape(samples, size, size, 1)
 train_samples = 10000
 X_train, X_test = X[:train_samples], X[train_samples:]
 Y_train, Y_test = Y[:train_samples], Y[train_samples:]
+
+model = Sequential()
+model.add(Conv2D(3323, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+model.add(Dropout(rate=0.6))
+model.add(MaxPooling2D(pool_size=(2, 2)))
