6c548916afbfc99c53a8e6c74c949fffbcbe14a7
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-14 23:26:57 -0400

    Train/Test split

--- cnn/mcts_go_mlp.py
+++ cnn/mcts_go_mlp.py
@@ -6,3 +6,14 @@ np.random.seed(123)
 # load sample data into np arrays
 X = np.load('../generated_games/features-40k.npy')
 Y = np.load('../generated_games/labels-40k.npy')
+samples = X.shape[0]
+board_size = 9*9
+
+# Transform input into vectors of size 81 instead of 9x9
+X = X.reshape(samples, board_size)
+Y = Y.reshape(samples, board_size)
+
+# Train on 90% of data, test on 10%
+train_samples = int(0.8 * samples)
+X_train, X_test = X[:train_samples], X[train_samples:]
+Y_train, Y_test = Y[:train_samples], Y[train_samples:] 
