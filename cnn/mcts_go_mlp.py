a347061bdc90e8c6eea0612b17711a5ee99cda48
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-14 23:30:46 -0400

    Train and evaluate model

--- cnn/mcts_go_mlp.py
+++ cnn/mcts_go_mlp.py
@@ -19,8 +19,17 @@ X_train, X_test = X[:train_samples], X[train_samples:]
 Y_train, Y_test = Y[:train_samples], Y[train_samples:]
 
 model = Sequential()
+# Add 3 dense layers, since for 9x9 grid there are 81 moves, we need 81 classes
 model.add(Dense(1000, activation='sigmoid', input_shape=(board_size,)))
 model.add(Dense(500, activation='sigmoid'))
 model.add(Dense(board_size, activation='sigmoid'))
 model.summary()
- 
+
+model.compile(loss='mean_squared_error', optimizer='sgdd', metrics=['accuracy'])
+
+# Fit the model
+model.fit(X_train, Y_train, batch_size=64, epochs=15, verbose=1, validation_data=(X_test, Y_test))
+
+score = model.evaluate(X_test, Y_test, verbose=0)
+Print("Test loss: ", score[0])
+Print("Test Accuracy: ", score[1])
