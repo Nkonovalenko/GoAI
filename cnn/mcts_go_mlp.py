728cec6675881d3c8d3d8a4e2d853d18c310ee21
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-15 00:09:47 -0400

    Fix typos

--- cnn/mcts_go_mlp.py
+++ cnn/mcts_go_mlp.py
@@ -25,11 +25,11 @@ model.add(Dense(500, activation='sigmoid'))
 model.add(Dense(board_size, activation='sigmoid'))
 model.summary()
 
-model.compile(loss='mean_squared_error', optimizer='sgdd', metrics=['accuracy'])
+model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
 
 # Fit the model
 model.fit(X_train, Y_train, batch_size=64, epochs=15, verbose=1, validation_data=(X_test, Y_test))
 
 score = model.evaluate(X_test, Y_test, verbose=0)
-Print("Test loss: ", score[0])
-Print("Test Accuracy: ", score[1])
+print("Test loss: ", score[0])
+print("Test Accuracy: ", score[1])
