d74fffe50fca11d7f9a6af1e18f8cfd309d15452
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-16 00:32:03 -0400

    Load go data

--- dlgo/data/processor.py
+++ dlgo/data/processor.py
@@ -21,3 +21,25 @@ class GoDataProcessor:
         self.encoder = get_encoder_by_name(encoder, 19)
         self.data_dir = data_directory
 
+    def load_go_data(self, data_type='train', num_samples=1000):
+        index = KGSIndex(data_directory=self.data_dir)
+        index.download_files()
+        sampler = Sampler(data_dir=self.data_dir)
+        data = sampler.draw_data(data_type, num_samples)
+
+        zip_names = set()
+        indices_by_zip_name = {}
+
+        for filename, index in data:
+            zip_names.add(filename)
+            if filename not in indices_by_zip_name:
+                indices_by_zip_name[filename] = []
+            indices_by_zip_name[filename].append(index)
+        for zip_name in zip_names:
+            base_name = zip_names.replace('.tar.gz', '')
+            data_file_name = base_name + data_type
+            if not os.path.isfile(self.data_dir + '/' + data_file_name):
+                self.process_zip(zip_name, data_file_name, indices_by_zip_name[zip_name])
+
+        features_and_labels = self.consolidate_games(data_type, data)
+        return features_and_labels
