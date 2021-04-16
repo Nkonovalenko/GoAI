521184337be3df1a5e647cb53781a6c58a451cf1
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-16 00:14:36 -0400

    Download kgs files

--- dlgo/data/index_processor.py
+++ dlgo/data/index_processor.py
@@ -8,17 +8,6 @@ if sys.version_info[0] == 3:
 else:
     from urllib import urlopen, urlretrieve
 
-
-
-def worker(url_and_target):  # Parallelize data download via multiprocessing
-    try:
-        (url, target_path) = url_and_target
-        print('>>> Downloading ' + target_path)
-        urlretrieve(url, target_path)
-    except (KeyboardInterrupt, SystemExit):
-        print('>>> Exiting child process')
-
-
 class KGSIndex:
 
     def __init__(self,
@@ -63,40 +52,6 @@ class KGSIndex:
             pool.terminate()
             pool.join()
             sys.exit(-1)
-
-    def create_index_page(self):
-        """If there is no local html containing links to files, create one."""
-        if os.path.isfile(self.index_page):
-            print('>>> Reading cached index page')
-            index_file = open(self.index_page, 'r')
-            index_contents = index_file.read()
-            index_file.close()
-        else:
-            print('>>> Downloading index page')
-            fp = urlopen(self.kgs_url)
-            data = six.text_type(fp.read())
-            fp.close()
-            index_contents = data
-            index_file = open(self.index_page, 'w')
-            index_file.write(index_contents)
-            index_file.close()
-        return index_contents
-
-    def load_index(self):
-        """Create the actual index representation from the previously downloaded or cached html."""
-        index_contents = self.create_index_page()
-        split_page = [item for item in index_contents.split('<a href="') if item.startswith("https://")]
-        for item in split_page:
-            download_url = item.split('">Download')[0]
-            if download_url.endswith('.tar.gz'):
-                self.urls.append(download_url)
-        for url in self.urls:
-            filename = os.path.basename(url)
-            split_file_name = filename.split('-')
-            num_games = int(split_file_name[len(split_file_name) - 2])
-            print(filename + ' ' + str(num_games))
-            self.file_info.append({'url': url, 'filename': filename, 'num_games': num_games})
-
 
 if __name__ == '__main__':
     index = KGSIndex()
