480c36919d2098b2271a6977fd0b485c94a8c7ea
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-17 23:32:32 -0400

    Add imports

---
+++ dlgo/data/parallel_processor.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+from __future__ import absolute_import
+import os
+import glob
+import os.path
+import tarfile
+import gzip
+import shutil
+import numpy as np
+import multiprocessing
+from os import sys
+from keras.utils import to_categorical
+
+from dlgo.gosgf import Sgf_game
+from dlgo.goboard_fast import Board, GameState, Move
+from dlgo.gotypes import Player, Point
+from dlgo.data.index_processor import KGSIndex
+from dlgo.data.sampling import Sampler
+from dlgo.data.generator import DataGenerator
+from dlgo.encoders.base import get_encoder_by_name
+
