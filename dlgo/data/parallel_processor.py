330abc874ccabe38e8f4b13005488048d470f4a8
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-17 23:33:09 -0400

    Load data

--- dlgo/data/parallel_processor.py
+++ dlgo/data/parallel_processor.py
@@ -34,4 +34,21 @@ class GoDataProcessor:
         self.encoder = get_encoder_by_name(encoder, 19)
         self.data_dir = data_directory
 
+# tag::load_generator[]
+    def load_go_data(self, data_type='train', num_samples=1000,
+                     use_generator=False):
+        index = KGSIndex(data_directory=self.data_dir)
+        index.download_files()
+
+        sampler = Sampler(data_dir=self.data_dir)
+        data = sampler.draw_data(data_type, num_samples)
+
+        self.map_to_workers(data_type, data)  # <1>
+        if use_generator:
+            generator = DataGenerator(self.data_dir, data)
+            return generator  # <2>
+        else:
+            features_and_labels = self.consolidate_games(data_type, data)
+            return features_and_labels  # <3>
+
 
