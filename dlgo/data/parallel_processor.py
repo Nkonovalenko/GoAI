f77982581da5be6f5f4d18fa2b433d8506f233f3
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-17 23:33:38 -0400

    Process zip

--- dlgo/data/parallel_processor.py
+++ dlgo/data/parallel_processor.py
@@ -66,4 +66,129 @@ class GoDataProcessor:
         this_tar.close()
         return tar_file
 
+    def process_zip(self, zip_file_name, data_file_name, game_list):
+        tar_file = self.unzip_data(zip_file_name)
+        zip_file = tarfile.open(self.data_dir + '/' + tar_file)
+        name_list = zip_file.getnames()
+        total_examples = self.num_total_examples(zip_file, game_list, name_list)
+
+        shape = self.encoder.shape()
+        feature_shape = np.insert(shape, 0, np.asarray([total_examples]))
+        features = np.zeros(feature_shape)
+        labels = np.zeros((total_examples,))
+
+        counter = 0
+        for index in game_list:
+            name = name_list[index + 1]
+            if not name.endswith('.sgf'):
+                raise ValueError(name + ' is not a valid sgf')
+            sgf_content = zip_file.extractfile(name).read()
+            sgf = Sgf_game.from_string(sgf_content)
+
+            game_state, first_move_done = self.get_handicap(sgf)
+
+            for item in sgf.main_sequence_iter():
+                color, move_tuple = item.get_move()
+                point = None
+                if color is not None:
+                    if move_tuple is not None:
+                        row, col = move_tuple
+                        point = Point(row + 1, col + 1)
+                        move = Move.play(point)
+                    else:
+                        move = Move.pass_turn()
+                    if first_move_done and point is not None:
+                        features[counter] = self.encoder.encode(game_state)
+                        labels[counter] = self.encoder.encode_point(point)
+                        counter += 1
+                    game_state = game_state.apply_move(move)
+                    first_move_done = True
+
+        feature_file_base = self.data_dir + '/' + data_file_name + '_features_%d'
+        label_file_base = self.data_dir + '/' + data_file_name + '_labels_%d'
+
+        chunk = 0  # Due to files with large content, split up after chunksize
+        chunksize = 1024
+        while features.shape[0] >= chunksize:
+            feature_file = feature_file_base % chunk
+            label_file = label_file_base % chunk
+            chunk += 1
+            current_features, features = features[:chunksize], features[chunksize:]
+            current_labels, labels = labels[:chunksize], labels[chunksize:]
+            np.save(feature_file, current_features)
+            np.save(label_file, current_labels)
+
+    def consolidate_games(self, name, samples):
+        files_needed = set(file_name for file_name, index in samples)
+        file_names = []
+        for zip_file_name in files_needed:
+            file_name = zip_file_name.replace('.tar.gz', '') + name
+            file_names.append(file_name)
+
+        feature_list = []
+        label_list = []
+        for file_name in file_names:
+            file_prefix = file_name.replace('.tar.gz', '')
+            base = self.data_dir + '/' + file_prefix + '_features_*.npy'
+            for feature_file in glob.glob(base):
+                label_file = feature_file.replace('features', 'labels')
+                x = np.load(feature_file)
+                y = np.load(label_file)
+                x = x.astype('float32')
+                y = to_categorical(y.astype(int), 19 * 19)
+                feature_list.append(x)
+                label_list.append(y)
+
+        features = np.concatenate(feature_list, axis=0)
+        labels = np.concatenate(label_list, axis=0)
+
+        feature_file = self.data_dir + '/' + name
+        label_file = self.data_dir + '/' + name
+
+        np.save(feature_file, features)
+        np.save(label_file, labels)
+
+        return features, labels
+
+    @staticmethod
+    def get_handicap(sgf):  # Get handicap stones
+        go_board = Board(19, 19)
+        first_move_done = False
+        move = None
+        game_state = GameState.new_game(19)
+        if sgf.get_handicap() is not None and sgf.get_handicap() != 0:
+            for setup in sgf.get_root().get_setup_stones():
+                for move in setup:
+                    row, col = move
+                    go_board.place_stone(Player.black, Point(row + 1, col + 1))  # black gets handicap
+            first_move_done = True
+            game_state = GameState(go_board, Player.white, None, move)
+        return game_state, first_move_done
+
+    def map_to_workers(self, data_type, samples):
+        zip_names = set()
+        indices_by_zip_name = {}
+        for filename, index in samples:
+            zip_names.add(filename)
+            if filename not in indices_by_zip_name:
+                indices_by_zip_name[filename] = []
+            indices_by_zip_name[filename].append(index)
+
+        zips_to_process = []
+        for zip_name in zip_names:
+            base_name = zip_name.replace('.tar.gz', '')
+            data_file_name = base_name + data_type
+            if not os.path.isfile(self.data_dir + '/' + data_file_name):
+                zips_to_process.append((self.__class__, self.encoder_string, zip_name,
+                                        data_file_name, indices_by_zip_name[zip_name]))
+
+        cores = multiprocessing.cpu_count()  # Determine number of CPU cores and split work load among them
+        pool = multiprocessing.Pool(processes=cores)
+        p = pool.map_async(worker, zips_to_process)
+        try:
+            _ = p.get()
+        except KeyboardInterrupt:  # Caught keyboard interrupt, terminating workers
+            pool.terminate()
+            pool.join()
+            sys.exit(-1)
 
