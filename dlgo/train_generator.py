c25df835fef05f11971f39d812e0b792ab5770f0
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-18 23:50:45 -0400

    Create train/test generators

--- dlgo/train_generator.py
+++ dlgo/train_generator.py
@@ -5,3 +5,17 @@ from dlgo.networks import small
 from keras.models import Sequential
 from keras.layers.core import Dense
 from keras.callbacks import ModelCheckpoint
+
+go_board_rows, go_board_cols = 19, 19
+num_classes = go_board_rows * go_board_cols
+num_games = 100
+
+# Create encoder of 19x19 size 
+encoder = OnePlaneEncoder((go_board_rows, go_board_cols))
+
+#Initialize Go data processor
+processor = GoDataProcessor(encoder=encoder.name())
+
+# Create 2 generators, 1 for training, 1 for testing
+generator = processor.load_go_data('train', num_games, use_generator=True)
+test_generator = processor.load_go_data('test', num_games, use_generator=True) 
