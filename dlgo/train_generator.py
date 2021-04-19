8fe7a0786390371d65074a5f2ea6dbc2bc7f9358
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-18 23:48:43 -0400

    Handle imports

---
+++ dlgo/train_generator.py
@@ -0,0 +1,7 @@
+from dlgo.data.parallel_processor import GoDataProcessor
+from dlgo.encoders.oneplane import OnePlaneEncoder
+
+from dlgo.networks import small
+from keras.models import Sequential
+from keras.layers.core import Dense
+from keras.callbacks import ModelCheckpoint
