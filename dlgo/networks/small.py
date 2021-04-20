947e58ab8dc5c2354161aca1eb7e3faac110ecbe
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-19 21:45:37 -0400

    Fix typo

--- dlgo/networks/small.py
+++ dlgo/networks/small.py
@@ -3,16 +3,23 @@ from keras.layers.convolutional import Conv2D, ZeroPadding2D
 
 def layers(input_shape):
     return [
-        ZeroPadding2D(padding=3, input_shape=input_shape, data_format='channels_first'),
-        Conv2D(48, (7,7), data_format='channels_first'),
-        Conv2D(323, (5,5), data_format='channels_first'),
+        ZeroPadding2D(padding=3, input_shape=input_shape, data_format='channels_last'),
+        Conv2D(48, (7,7), data_format='channels_last'),
         Activation('relu'),
 
-        ZeroPadding2D(padding=2, data_format='channels_first'),
-        Conv2D(32, (5,5), data_format='channels_first'),
+        ZeroPadding2D(padding=2, data_format='channels_last'),
+        Conv2D(32, (5,5), data_format='channels_last'),
+        Activation('relu'),
+
+        ZeroPadding2D(padding=2, data_format='channels_last'),
+        Conv2D(32, (5,5), data_format='channels_last'),
+        Activation('relu'),
+
+        ZeroPadding2D(padding=2, data_format='channels_last'),
+        Conv2D(32, (5,5), data_format='channels_last'),
         Activation('relu'),
 
         Flatten(),
         Dense(512),
-        Activation('relu')
+        Activation('relu'),
     ]
