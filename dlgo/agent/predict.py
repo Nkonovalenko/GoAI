c8d25e6f62ee7179b72756dff5930e20c89f7d6b
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-22 00:00:02 -0400

    Apply clipping

--- dlgo/agent/predict.py
+++ dlgo/agent/predict.py
@@ -19,3 +19,9 @@ class DeepLearningAgent(agent):
     def select_move(self, game_state):
         num_moves = self.encoder.board_width * self.encoder.board_height
         move_probs = self.predict(game_state)
+        # Increase the distance between the more likely and least likely moves
+        move_probs = move_probs ** 3
+        eps = 1e-6
+        # prevent move probabilities from getting stuck at 0 or 1
+        move_probs = np.clip(move_probs, eps, 1 - eps)
+        move_probs = move_probs / np.sum(move_probs)
