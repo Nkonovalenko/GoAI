ea102ec656906dd1fef4a8a218bc72a30f0ed55c
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-21 23:58:15 -0400

    Predict next move

--- dlgo/agent/predict.py
+++ dlgo/agent/predict.py
@@ -10,3 +10,9 @@ class DeepLearningAgent(agent):
         Agent.__init__(self)
         self.model = model
         self.encoder = encoder
+
+    def predict(self, game_state):
+        encoded_state = self.encoder.encode(game_state)
+        input_tensor = np.array([encoded_state])
+        return self.model.predict(input_tensor)[0]
+
