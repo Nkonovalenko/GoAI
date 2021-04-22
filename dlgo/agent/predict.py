5e1d5609fa679a945276102eda916bcadfc93779
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-21 23:56:33 -0400

    Initialize DeepLearningAgent

--- dlgo/agent/predict.py
+++ dlgo/agent/predict.py
@@ -4,3 +4,9 @@ from dlgo.agent.base import Agent
 from dlgo.agent.helpers import is_point_an_eye
 from dlgo import encoders
 from dlgo import goboard
+
+class DeepLearningAgent(agent):
+    def __init__(self, model, encoder):
+        Agent.__init__(self)
+        self.model = model
+        self.encoder = encoder
