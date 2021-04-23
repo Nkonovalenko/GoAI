e53bb1ef027b15589e4d68e53d66bcf671019e25
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-22 23:48:01 -0400

    Load model from hdf5

--- dlgo/kerasutil.py
+++ dlgo/kerasutil.py
@@ -18,3 +18,20 @@ def save_model_to_hdf5_group(model, f):
         serialized_model.close()
     finally:
         os.unlink(tempfname)
+
+def load_model_from_hdf5_group(f, custom_objects=None):
+    # Extract the model into a temporary file. Then we can use Keras load_model to read it
+    tmpfd, tempfname = tempfile.mkstemp(prefix='tmp-kerasmodel')
+    try:
+        os.close(tempfd)
+        serialized_model = h5py.File(tempfname, 'w')
+        root_item = f.get('kerasmodel')
+        for attr_name, attr_value in root_item.attrs.items():
+            serialized_model.attrs[attr_name] = attr_value
+        for k in root_item.keys():
+            f.copy(root_item.get(k), serialized_model, k)
+        serialized_model.close()
+        return load_model(tempfname, custom_objects=custom_objects)
+    finally:
+        os.unlink(tempfname)
+
