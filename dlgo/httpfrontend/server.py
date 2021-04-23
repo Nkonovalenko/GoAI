714579626afbf063b452cdd9de88ec4899518d27
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-22 23:59:15 -0400

    Update server.py

--- dlgo/httpfrontend/server.py
+++ dlgo/httpfrontend/server.py
@@ -35,4 +35,26 @@ def get_web_app(bot_map):
         context = request.json
         board_size = content['board_size']
         game_state = goboard.GameState.new_game(board_size)
-        
+        # Reply the game up to this point
+        for move in content['moves']:
+            if move == 'pass':
+                next_move = goboard.Move.pass_turn()
+            elif move == 'resign':
+                next_move = goboard.Move.resign()
+            else:
+                next_move = goboard.Move.play(point_from_coords(move))
+            game_state = game_state.apply_move(next_move)
+        bot_agent = bot_map[bot_name]
+        bot_move = bot_agent.select_move(game_state)
+        if bot_move.is_pass:
+            bot_move_str = 'pass'
+        elif bot_move.is_resign:
+            bot_move_str = 'resign'
+        else:
+            bot_move_str = coords_from_point(bot_move.point)
+
+        return jsonify({
+            'bot_move': bot_move_str,
+            'diagnostics': bot_agent.diagnostics()
+        })
+    return app
