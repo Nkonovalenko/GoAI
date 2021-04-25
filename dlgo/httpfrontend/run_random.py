14ec62e62790f23d792e890fd122f71367fa0776
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-24 23:58:00 -0400

    Create run_random_http.py

---
+++ dlgo/httpfrontend/run_random.py
@@ -0,0 +1,7 @@
+# Navigate to http://127.0.0.1:5000/static/play_random_99.html
+from dlgo.agent.naive import RandomBot
+from dlgo.httpfrontend.server import get_web_app
+
+random_agent = RandomBot()
+web_app = get_web_app({'random': random_agent})
+web_app.run()
