e606ce7a52d97b474472379051277420ef4bf7a2
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-25 23:43:57 -0400

    Finish end to end

--- end_to_end.py
+++ end_to_end.py
@@ -13,3 +13,26 @@ go_board_rows, go_board_cols = 19, 19
 nb_classes = go_board_rows * go_board_cols
 encoder = SevenPlaneEncoder((go_board_rows, go_board_cols))
 processor = GoDataProcessor(encoder=encoder.name())
+
+X, y = processor.load_go_data(num_samples=100)
+
+input_shape = (encoder.num_planes, go_board_rows, go_board_cols)
+model = Sequential()
+network_layers = large.layers(input_shape)
+for layer in network_layers:
+    model.add(layer)
+model.add(Dense(nb_classes, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+
+model.fit(X, y, batch_size=128, epochs=20, verbose=1)
+
+# After training, create Go Bot and save in HDF5
+deep_learning_bot = DeepLearningAgent(model, encoder)
+deep_learning_bot.serialize("../agents/deep_bot.h5")
+
+# Load the bot into a web application
+model_file = h5py.File("../agents/deep_bot.h5", 'r')
+bot_from_file = load_prediction_agent(model_file)
+
+web_app = get_web_app({'predict': bot_from_file})
+web_app.run()
