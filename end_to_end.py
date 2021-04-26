aaed0e5ce8a0777c13cf7f6d0f68127dbfbc73b4
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-25 23:39:27 -0400

    Set up processor

--- end_to_end.py
+++ end_to_end.py
@@ -8,3 +8,8 @@ from dlgo.data.parallel_processor import GoDataProcessor
 from dlgo.encoders.sevenplane import SevenPlaneEncoder
 from dlgo.httpfrontend import get_web_app
 from dlgo.networks import large
+
+go_board_rows, go_board_cols = 19, 19
+nb_classes = go_board_rows * go_board_cols
+encoder = SevenPlaneEncoder((go_board_rows, go_board_cols))
+processor = GoDataProcessor(encoder=encoder.name())
