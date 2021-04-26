924d48e65e33a401a8c7412f01f660aa4a687301
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-25 23:33:01 -0400

    Create sevenplane class

--- dlgo/encoders/sevenplane.py
+++ dlgo/encoders/sevenplane.py
@@ -3,3 +3,10 @@ import numpy as np
 from dlgo.encoders.base import Encoder
 from dlgo.goboard import Move, Point
 
+class SevenPlaneEncoder(Encoder):
+    def __init__(self, board_size):
+        self.board_width, self.board_height = board_size
+        self.num_planes = 7
+
+    def name(self):
+        return 'sevenplane'
