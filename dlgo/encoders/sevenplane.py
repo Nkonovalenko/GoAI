6c9e8e1dfd7c71a38130af02c1cb043e0dfa5a28
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-04-25 23:37:22 -0400

    Decode point index

--- dlgo/encoders/sevenplane.py
+++ dlgo/encoders/sevenplane.py
@@ -30,4 +30,8 @@ class SevenPlaneEncoder(Encoder):
 
     def encode_point(self, point):
         return self.board_width * (point.row - 1) + (point.col - 1)
-    
+
+    def decode_point_index(self, index):
+        row = index // self.board_width
+        col = index % self.board_width
+        return Point(row=row+1, col=col+1)
