53adfa7636f2dbd4b0ed469a8530e5fb3a126f15
Nkonovalenko <43218639+Nkonovalenko@users.noreply.github.com>
2021-05-30 18:25:39 -0400

    Success response

--- dlgo/gtp/response.py
+++ dlgo/gtp/response.py
@@ -9,3 +9,7 @@ class Response:
     def __init__(self, status, body):
         self.success = status
         self.body = body
+
+def success(body=''):
+    return Response(status=True, body=body)
+
